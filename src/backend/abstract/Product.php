e09e68d1c24e3e7129e5445794efdb1c4e98509a
Mohammad AlDandashi <m7amad_00@hotmail.com>
2023-01-30 01:55:29 +0200

    Update Product.php

--- src/backend/abstract/Product.php
+++ src/backend/abstract/Product.php
@@ -55,7 +55,6 @@ class Product
 	}
 
 	//made it static because i am not accessing anything special in the class itself.
-
 	public static function removeProducts($connection, $table, $selectedProducts)
 	{
 		foreach ($selectedProducts as $product) {
